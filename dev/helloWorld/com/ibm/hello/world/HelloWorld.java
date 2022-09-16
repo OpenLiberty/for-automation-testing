0de4b81eccc9ca08a27fc4fa51a4dc0afcab177b
Wendy Raschke <wraschke@us.ibm.com>
2022-09-16 12:04:40 -0500

    A sample commit

--- dev/helloWorld/com/ibm/hello/world/HelloWorld.java
+++ dev/helloWorld/com/ibm/hello/world/HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 		System.out.println("Hello, world.");
 	}
+	
+	public static String getThis(String echo) {
+		System.out.println("Now hear this!");
+		return echo;
 }
